8ebeddff67495294c0f86ba38e3a7f0e3564b891
jiunlee <jiunlee.dev@gmail.com>
2020-11-17 15:48:47 +0900

    MVC게시판

--- model2Test/src/controller/BoardFrontController.java
+++ model2Test/src/controller/BoardFrontController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
+import action.BoardListAction;
 import action.BoardWriteProAction;
 import vo.ActionForward;
 
@@ -48,6 +49,13 @@ public class BoardFrontController extends HttpServlet {
 					forward = action.execute(request, response);
 					} catch (Exception e) {
 						e.printStackTrace();
+					}
+				} else if(command.equals("/boardList.do")) {
+							action = new BoardListAction();
+							try {
+							forward = action.execute(request, response);
+							} catch (Exception e) {
+								e.printStackTrace();
 					}
 				}
 			if(forward != null) {
